64dcb4b38d9dd14696f645a28848e1f51df78676
bc-uh <chern@hartford.edu>
2023-09-20 09:35:49 -0400

    Add temp conversion

--- Assignment1.java
+++ Assignment1.java
@@ -1,11 +1,19 @@
 // Import libraries here
 
+import java.util.Scanner;
+
 public class Assignment1 {
   public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
     System.out.println("BBBB    CCCC");
     System.out.println("B   B  C    ");
     System.out.println("BBBB   C    ");
-    System.out.println("B   B  C    ")
-    System.out.println("BBBB    CCCC")
+    System.out.println("B   B  C    ");
+    System.out.println("BBBB    CCCC");
+    System.out.println("Enter a number in farenheit");
+    int farenheit = scanner.nextInt();
+    int celsius = (5/9) * (farenheit - 32);
   }
 }
