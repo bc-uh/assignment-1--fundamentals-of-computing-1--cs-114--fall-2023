752fa6291475b571eca1285ce262e795db838945
bc-uh <chern@hartford.edu>
2023-09-20 18:04:23 -0400

    Reformat input prompts

--- Assignment1.java
+++ Assignment1.java
@@ -27,14 +27,14 @@ public class Assignment1 {
   }
   public static String stringPrompt() {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Input a 5 character string");
+    System.out.println("Input a 5 character string: ");
     String inputString = scanner.nextLine();
     String reversedString = new StringBuilder(inputString).reverse().toString();
     return reversedString.substring(1, 4);
   }
   public static double temperaturePrompt() {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Input temperature in fahrenheit");
+    System.out.println("Input temperature in fahrenheit: ");
     return scanner.nextDouble() * (5/9) + 32;
   }
   public static int randomNumberGenerator() {
